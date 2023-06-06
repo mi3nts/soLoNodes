79d5db0fb797095241fc3f5c0fbe27079c319da5
lakihaomal <lhw150030@utdallas.edu>
2023-06-06 14:23:34 -0500

    first update

---
+++ firmware/xu4LoRa/sudoRun.sh
@@ -0,0 +1,13 @@
+
+sleep 20
+sudo uhubctl -a off -p 1 -l 1-1
+sleep 5 
+sudo uhubctl -a on -p 1 -l 1-1
+sleep 5
+sudo uhubctl
+sleep 2 
+sudo chmod 777 /dev/tty*
+sleep 2
+sudo chmod 777 /dev/vid*
+sleep 2
+sudo chmod 777 /dev/i2c*
