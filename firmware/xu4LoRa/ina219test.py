69e4c8b54f453eba00dd1cb69b2d4412f4cc789d
lakihaomal <lhw150030@utdallas.edu>
2023-06-25 17:39:55 -0500

    gps power saving updates

--- firmware/xu4LoRa/ina219test.py
+++ firmware/xu4LoRa/ina219test.py
@@ -3,41 +3,43 @@ from ina219 import DeviceRangeError
 import odroid_wiringpi as wpi
 wpi.wiringPiSetup()
 
-
+import time
 
 SHUNT_OHMS = 0.1
 MAX_EXPECTED_AMPS = 0.2
 
 
 def read():
-
-    batteryLevelRaw = wpi.analogRead(29)
-    print(batteryLevelRaw)
-    ina = INA219(SHUNT_OHMS, busnum=2)
-    ina.configure()
-    print("Adafruit_GPIO.I2C" in str(ina._i2c))
-
-    ina2 = INA219(SHUNT_OHMS, address=0x41, busnum=1)
-    ina2.configure()
-    print(ina2._i2c)
-
-    print("Bus Voltage: %.3f V" % ina.voltage())
     try:
-        print("Bus Current: %.3f mA" % ina.current())
-        print("Power: %.3f mW" % ina.power())
-        print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
-    except DeviceRangeError as e:
-        print("Current overflow")
+        batteryLevelRaw = wpi.analogRead(29)
+        print(batteryLevelRaw)
+        inaSolarOut   = INA219(SHUNT_OHMS, busnum=1)
+        inaBatteryOut = INA219(SHUNT_OHMS, address=0x41, busnum=1)
+
+        print("Adafruit_GPIO.I2C" in str(inaSolarOut._i2c))
+
+        inaSolarOut.configure()
+        inaBatteryOut.configure()
+    
 
 
-    print("Bus Voltage: %.3f V" % ina2.voltage())
-    try:
-        print("Bus Current: %.3f mA" % ina2.current())
-        print("Power: %.3f mW" % ina2.power())
-        print("Shunt voltage: %.3f mV" % ina2.shunt_voltage())
-    except DeviceRangeError as e:
-        print("Current overflow")
+        print("Bus Voltage: %.3f V" % inaSolarOut.voltage())
+        print("Bus Current: %.3f mA" % inaSolarOut.current())
+        print("Power: %.3f mW" % inaSolarOut.power())
+        print("Shunt voltage: %.3f mV" % inaSolarOut.shunt_voltage())
 
+
+        print("Bus Voltage: %.3f V" % inaBatteryOut.voltage())
+        print("Bus Current: %.3f mA" % inaBatteryOut.current())
+        print("Power: %.3f mW" % inaBatteryOut.power())
+        print("Shunt voltage: %.3f mV" % inaBatteryOut.shunt_voltage())
+    
+    except Exception as e:
+        time.sleep(.5)
+        print ("Error and type: %s - %s." % (e,type(e)))
+        time.sleep(.5)
+        print("Data Packet Not Sent for PM")
+        time.sleep(.5)
 
 
 if __name__ == "__main__":
